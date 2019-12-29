9e219d761a6027a49c9dd2b346a7001f00adc26e
Yoshihiro <52352420+Yoshihiro1989@users.noreply.github.com>
2019-12-29 21:46:28 +0900

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,resolve_url
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.generic import DetailView,UpdateView
+from .form import UserForm
 # Create your views here.
 
 
@@ -24,3 +28,19 @@ def signup(request):
         "form": form
     }
     return render(request, 'app/signup.html', context)
+@login_required
+def home(request):
+    return render(request, "app/home.html")
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = "app/users/detail.html"
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = "app/users/update.html"
+    form_class = UserForm
+
+    def get_success_url(self):
+        return resolve_url('app:users_detail', pk=self.kwargs['pk'])
